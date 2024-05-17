bddc91de387058eab373523fac08f28dd4a125c3
mariajvieira <up202204802@up.pt>
2024-05-17 20:48:57 +0100

    Final

--- SVGElements.cpp
+++ SVGElements.cpp
@@ -65,11 +65,9 @@ namespace svg
     }
 
 
-    // PERCEBER!!!!!!!!!!!!!!!
     void Ellipse::scale(int v,Point &t) {
         center = center.translate({-t.x, -t.y}).scale({0, 0}, v);
         center = center.translate(t);
-    
         radius = radius.scale({0, 0}, v);
     }
 
